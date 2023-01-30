20146a9db8ded7959ad02017eddaec81dc51eef9
{KhisamovDenis} <{khisamovdenis@icloud.com}>
2023-01-30 19:28:58 +0800

    Added new text

---
+++ src/com/otus/homework/oop/learning/Exercise.java
@@ -0,0 +1,26 @@
+package com.otus.homework.oop.learning;
+
+public class Exercise {
+
+    String question;
+    String[] answers;
+
+    int correctAnswers;
+
+    int correctCount = 0, wrongCount = 0;
+
+    public Exercise(String question,String[] answers,int correctAnswers ){
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswers = correctAnswers;
+      //  this.correctCount = correctCount;
+
+
+
+    }
+
+
+
+
+
+}
